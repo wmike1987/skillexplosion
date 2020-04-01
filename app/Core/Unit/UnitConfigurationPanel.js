287c4c83dfbecd2719fe6c0a9130ecee3230a9a5
wmike1987 <wmike1987@gmail.com>
2020-04-01 00:36:57 -0500

    Further the development of the equip station UI

--- app/Core/Unit/UnitConfigurationPanel.js
+++ app/Core/Unit/UnitConfigurationPanel.js
@@ -1,12 +1,16 @@
 define(['jquery', 'utils/GameUtils', 'core/Tooltip', 'matter-js'], function($, utils, Tooltip, Matter) {
 
+    var equipShow = utils.getSound('openEquipStation.wav', {volume: .1, rate: 1});
+    var equip = utils.getSound('augmentEquip.wav', {volume: .1, rate: 1.2});
+
     var ConfigPanel = function() {
     }
 
     ConfigPanel.prototype.initialize = function() {
         this.id = utils.uuidv4();
 
-        this.augmentOffsetY = -100;
+        this.initialYOffset = -31;
+        this.spacing = -65;
         $('body').on('keydown.unitConfigurationPanel', function( event ) {
             var key = event.key.toLowerCase();
             if(key == 'escape') {
@@ -16,10 +20,11 @@ define(['jquery', 'utils/GameUtils', 'core/Tooltip', 'matter-js'], function($, u
 
         this.abilityBases = [];
         for(var x = 0; x < 3; x++) {
-            var base = utils.addSomethingToRenderer('TintableSquare', {where: 'hud', tint: 0xbbbbbb, anchor: {x: .5, y: 1}});
+            var base = utils.addSomethingToRenderer('AugmentArmPanel', {where: 'hud', anchor: {x: .5, y: 1}});
             base.visible = false;
-            utils.makeSpriteSize(base, {w: 30, h: 150});
+            // utils.makeSpriteSize(base, {w: 30, h: 150});
             this.abilityBases.push(base)
+            base.sortYOffset = 1000;
         }
     }
 
@@ -27,6 +32,7 @@ define(['jquery', 'utils/GameUtils', 'core/Tooltip', 'matter-js'], function($, u
         this.hideForUnit(this.currentUnit);
 
         this.currentUnit = unit;
+        equipShow.play();
         this.showAugments(unit);
         //this.showStats
     };
@@ -34,23 +40,41 @@ define(['jquery', 'utils/GameUtils', 'core/Tooltip', 'matter-js'], function($, u
     ConfigPanel.prototype.showAugments = function(unit) {
 
         $.each(unit.abilities, function(i, ability) {
+            var currentAugment = ability.currentAugment;
             if(ability.augments) {
+                ability.currentAugmentBorder = utils.addSomethingToRenderer('AugmentBorderGold', "hudOne");
+                ability.currentAugmentBorder.visible = false;
+                ability.currentAugmentBorder.sortYOffset = 1000;
                 $.each(ability.augments, function(j, augment) {
                     if(!augment.icon.parent) {
-                        utils.addSomethingToRenderer(augment.icon, {position: {x: ability.position.x, y:ability.position.y + this.augmentOffsetY*(j+1)}, where: 'hudOne'});
+                        utils.addSomethingToRenderer(augment.icon, {position: {x: ability.position.x, y:utils.getPlayableHeight() + this.initialYOffset + this.spacing*(j)}, where: 'hudOne'});
+                        augment.border = utils.addSomethingToRenderer('AugmentBorder', {position: {x: ability.position.x, y:utils.getPlayableHeight() + this.initialYOffset + this.spacing*(j)}, where: 'hudOne'});
                         augment.icon.interactive = true;
                         augment.icon.on('mouseup', function(event) {
                             ability.currentAugment = augment;
-                        }.bind(this))
+                            ability.currentAugmentBorder.position = this.position;
+                            ability.currentAugmentBorder.visible = true;
+                            equip.play();
+                        })
                         Tooltip.makeTooltippable(augment.icon, {title: augment.title});
 
+                    }
+                    if(currentAugment == augment) {
+                        augment.icon.alpha = 1;
+                        augment.border.alpha = 0;
+                        augment.border.visible = false;
+                        ability.currentAugmentBorder.visible = true;
+                        ability.currentAugmentBorder.position = augment.border.position;
+                    } else {
+                        augment.icon.alpha = .8;
+                        augment.border.alpha = 1;
+                        augment.border.visible = true;
                     }
                     augment.icon.visible = true;
                 }.bind(this))
 
                 //show ability bases
-                utils.makeSpriteSize(this.abilityBases[i], {w: ability.icon.width, h: ability.augments.length * 100});
-                this.abilityBases[i].position = {x: ability.position.x, y: utils.getPlayableHeight()}
+                this.abilityBases[i].position = {x: ability.position.x, y: utils.getPlayableHeight()+1}
                 this.abilityBases[i].visible = true;
             }
         }.bind(this))
@@ -69,8 +93,12 @@ define(['jquery', 'utils/GameUtils', 'core/Tooltip', 'matter-js'], function($, u
             if(ability.augments) {
                 $.each(ability.augments, function(j, augment) {
                     augment.icon.visible = false;
+                    augment.border.visible = false;
                     augment.icon.tooltipObj.hide();
                 }.bind(this))
+            }
+            if(ability.currentAugmentBorder) {
+                ability.currentAugmentBorder.visible = false;
             }
         }.bind(this))
     };
