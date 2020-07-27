6359d3bd594e841b3646b9b706e222f41d2cbf12
Jeff Warner <65909511+jeffwarner02@users.noreply.github.com>
2020-07-27 14:26:50 -0500

    Updated UpDown

--- app/Games/UpDown_Practice.js
+++ app/Games/UpDown_Practice.js
@@ -1,25 +1,26 @@
-define(['jquery', 'matter-js', 'pixi', 'core/CommonGameMixin', 'utils/TargetSpawnZone'], function($, Matter, PIXI, CommonGameMixin, zone) {
-	
+define(['jquery', 'matter-js', 'pixi', 'core/CommonGameMixin', 'utils/TargetSpawnZone', 'utils/GameUtils'],
+function($, Matter, PIXI, CommonGameMixin, zone, utils) {
+
 	var targetScore = 1;
-	
+
 	var game = {
 		gameName: 'UpDown',
 		ball: null,
 		victoryCondition: {type: 'timed', limit: 35},
-		
+
 		initExtension: function() {
-		    this.hit = utils.getSound('nicehit1.wav', {volume: .2, rate: 2});  
-		    this.hit2 = utils.getSound('nicehit1.wav', {volume: .2, rate: 2.3});  
-		    this.hit3 = utils.getSound('nicehit1.wav', {volume: .2, rate: 2.6});  
-		    this.hit4 = utils.getSound('nicehit1.wav', {volume: .2, rate: 3});  
+		    this.hit = utils.getSound('nicehit1.wav', {volume: .2, rate: 2});
+		    this.hit2 = utils.getSound('nicehit1.wav', {volume: .2, rate: 2.3});
+		    this.hit3 = utils.getSound('nicehit1.wav', {volume: .2, rate: 2.6});
+		    this.hit4 = utils.getSound('nicehit1.wav', {volume: .2, rate: 3});
 		    this.hits = [this.hit, this.hit2, this.hit3, this.hit4];
 		},
-		
+
 		play: function(options) {
-		
+
 			this.ghostTarget = null;
 			this.chain = 0;
-			
+
 			//create zones
 			this.topZone =    new zone(this.canvas.width*3/8, 0, this.canvas.width/4, this.canvas.height/2, 40, 100);
 			this.bottomZone = new zone(this.canvas.width*3/8, this.canvas.height/2, this.canvas.width/4, this.canvas.height/2, 40, 100);
@@ -27,33 +28,40 @@ define(['jquery', 'matter-js', 'pixi', 'core/CommonGameMixin', 'utils/TargetSpaw
 			this.zoneManager= {zone: this.bottomZone, flip: function() {
 				this.zone = this.zone == self.bottomZone ? self.topZone : self.bottomZone;
 			}};
-			
+
 			//init a ball
 			this.ball = this.createNextBody();
-			this.addBody(this.ball);	
-			
-			//create ghost target indicator		
-			this.addEventListener('mousedown', function(event) { 
+			this.addBody(this.ball);
+
+			//create ghost target indicator
+			this.addEventListener('mousedown', function(event) {
 				this.ghostTarget = this.ghostTarget || utils.addSomethingToRenderer('blueTarget2Ghost', 'stageNTwo');
 				this.ghostTarget.position.x = this.ball.position.x;
 				this.ghostTarget.position.y = this.ball.position.y;
 				this.ghostTarget.scale.x = this.ghostTarget.scale.y = this.ball.circleRadius*2/128;
-			}.bind(this));			
-			
-			this.addEventListener('mousedown', function(event) { 
+			}.bind(this));
+
+			this.addEventListener('mousedown', function(event) {
 				var rect = this.canvasEl.getBoundingClientRect();
 				var x = event.clientX - rect.left;
 				var y = event.clientY - rect.top;
 				if(Matter.Bounds.contains(this.ball.bounds, {x: x, y: y})) {
 					if(Matter.Vertices.contains(this.ball.vertices, {x: x, y: y})) {
 						this.incrementScore(targetScore);
-						
+
 						//play death animation
-						this.getAnimation('blueCollapse', [this.ball.position.x, this.ball.position.y, (this.ball.circleRadius*2/512), (this.ball.circleRadius*2/512)], .6).play();
-						
+						//utils.getAnimation('blueCollapse', [this.ball.position.x, this.ball.position.y, (this.ball.circleRadius*2/512), (this.ball.circleRadius*2/512)], .6).play();
+						var collapseAnimation = utils.getAnimationB({
+							numberOfFrames: 8,
+							baseName: 'blueCollapse',
+							speed: 1,
+							transform: [this.ball.position.x, this.ball.position.y, this.ball.circleRadius*2/512, this.ball.circleRadius*2/512]
+						});
+						utils.addSomethingToRenderer(collapseAnimation);
+						collapseAnimation.play();
 						this.chain += 1;
 						this.hits[this.chain-1].play();
-						
+
 						//add time if we've chained three in a row
 						if(this.chain == 4) {
 						    this.chain = 0;
@@ -61,9 +69,9 @@ define(['jquery', 'matter-js', 'pixi', 'core/CommonGameMixin', 'utils/TargetSpaw
     						var plusOne = utils.addSomethingToRenderer('PlusOne', 'foreground');
     						plusOne.position = this.ball.position;
     						plusOne.position.y -= 50;
-    						this.floatSprite(plusOne);
+    						utils.floatSprite(plusOne);
 						}
-						
+
 						this.removeBody(this.ball);
 						this.addBody(this.ball = this.createNextBody());
 					} else { //miss
@@ -74,43 +82,34 @@ define(['jquery', 'matter-js', 'pixi', 'core/CommonGameMixin', 'utils/TargetSpaw
 				}
 			}.bind(this));
 		},
-		
+
 		createNextBody: function() {
 			var radius = Math.random() * 15 + 40; //radius between 40-55
 			var xLoc = this.zoneManager.zone.giveMeLocation().x;
 			var yLoc = this.zoneManager.zone.lastLocation.y;
 			this.zoneManager.flip();
 			this.ball = Matter.Bodies.circle(xLoc, yLoc, radius);
-			this.ball.render.sprite.texture = this.texture('blueTarget2');
-			this.ball.render.sprite.xScale = this.ball.render.sprite.yScale = radius*2/128;
-			
+			this.ball.renderChildren = [{
+                id: 'ball',
+                data: 'blueTarget2',
+                scale: {x:radius*2/128, y:radius*2/128},
+            }]
+			//this.ball.render.sprite.texture = this.texture('blueTarget2');
+			//this.ball.render.sprite.xScale = this.ball.render.sprite.yScale = radius*2/128;
+
 			return this.ball;
 		},
 	}
-	
+
 	/*
 	 * Options to for the game starter
-	 */ 
+	 */
 	game.worldOptions = {
 			background: {image: 'SteelBackground', scale: {x: 1.334, y: 1.334}},
-		        width: 1200, 
-		        height: 600, 
+		        width: 1200,
+		        height: 600,
 		        gravity: 0.000,
 		       };
-	
+
 	return $.extend({}, CommonGameMixin, game);
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
