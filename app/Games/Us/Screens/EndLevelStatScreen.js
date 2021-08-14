bc72ad1304f6484af02910056708e0d288bb4082
wmike1987 <wmike1987@gmail.com>
2021-08-14 15:15:50 -0500

    Updated stat text

--- app/Games/Us/Screens/EndLevelStatScreen.js
+++ app/Games/Us/Screens/EndLevelStatScreen.js
@@ -42,7 +42,7 @@ var dashesPerformed = "Dashes Performed";
 //Ursula titles
 var ursulaTitle = "Ursula";
 var minesLaid = "Mines Laid";
-var secretStepsPerformed = "Vanish";
+var secretStepsPerformed = "Vanishes Performed";
 
 var createContainer = function() {
     var container = new PIXI.Container();
